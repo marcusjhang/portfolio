a7a43d8b1fef015a84dc80ee030cdba221d1dda0
Marcus Ang <marcusajh0802@gmail.com>
2023-07-03 11:13:17 +0800

    fix dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,19 +79,19 @@ export default function Navbar() {
                   </Link>
                 )
               })}
-              {currentTheme === "dark" ? (
-                <button
-                  onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
-                  <RiSunLine size={25} color="black" />
-                </button>
-              ) : (
+              {currentTheme === "light" ? (
                 <button
                   onClick={() => setTheme("dark")}
                   className="bg-slate-100 p-2 rounded-xl"
                 >
                   <RiMoonFill size={25} />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
                 </button>
               )}
             </div>
